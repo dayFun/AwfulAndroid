59a3c85b4407d8991043d66279bfd2d4c8767f48
dayFun <dayFun@asynchrony.com>
2016-04-02 19:38:49 -0500

    android studio 2.0 stuff

--- app/src/main/java/com/lighthouse/awfulandroid/activities/instructions/InstructionsActivity.java
+++ app/src/main/java/com/lighthouse/awfulandroid/activities/instructions/InstructionsActivity.java
@@ -1,25 +1,20 @@
 package com.lighthouse.awfulandroid.activities.instructions;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.lighthouse.awfulandroid.R;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
 public class InstructionsActivity extends AppCompatActivity {
-
-    @Bind(R.id.instructions_viewpager)
-    ViewPager instructionsViewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions_pager);
-        ButterKnife.bind(this);
 
-        instructionsViewpager.setAdapter(new InstructionsPagerAdapter(getSupportFragmentManager()));
+        FragmentManager fm = getSupportFragmentManager();
+        DemoInstructionPageDialog overlay = new DemoInstructionPageDialog();
+        overlay.show(fm, "InstructionsDialog");
     }
 }
